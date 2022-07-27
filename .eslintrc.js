32b4a9f0610ff77970075740ad01c549425c9d6b
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-07-27 14:25:13 -0500

    Add entrypoint and eslint config.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  }
+};
