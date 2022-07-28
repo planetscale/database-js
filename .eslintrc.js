9a365ab3ba0a2148103682e12c41cdb9d4db6360
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-07-28 15:59:43 -0500

    Add more eslint and TS config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
   root: true,
   env: {
     browser: true,
